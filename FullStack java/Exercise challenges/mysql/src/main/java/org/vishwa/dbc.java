5c377a300051201064f535f924c16b0a86f07d87
selvaviswanath <selvaviswanath055@gmail.com>
2023-11-10 05:05:29 +0530

    jsp & sql

---
+++ FullStack java/Exercise challenges/mysql/src/main/java/org/vishwa/dbc.java
@@ -0,0 +1,57 @@
+package org.vishwa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/dbc")
+public class dbc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(name = "jdbc/project")
+	private DataSource dataSource;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		   PrintWriter out = response.getWriter();
+           Connection connect = null;
+           Statement stmt = null;
+           ResultSet rs = null;
+           try {
+			connect = dataSource.getConnection();
+			String query = "Select * from users";
+			stmt = connect.createStatement();
+            rs = stmt.executeQuery(query);
+            
+			while(rs.next()){
+				out.print("<br/>"+rs.getString("email"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
