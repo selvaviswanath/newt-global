a76063c12915e84baec84a37ba29380e7b4710b3
selvaviswanath <selvaviswanath055@gmail.com>
2023-11-11 14:27:28 +0530

    File uploading and hibernate

--- FullStack java/Exercise challenges/mysql/src/main/java/org/vishwa/dbc.java
+++ FullStack java/Exercise challenges/mysql/src/main/java/org/vishwa/dbc.java
@@ -1,23 +1,36 @@
 package org.vishwa;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-
 import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
-@WebServlet("/dbc")
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@WebServlet(name = "FileUploadServlet", urlPatterns = { "/fileuploadservlet" })
+@MultipartConfig(
+		  fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+		  maxFileSize = 1024 * 1024 * 10,      // 10 MB
+		  maxRequestSize = 1024 * 1024 * 100   // 100 MB
+		)
 public class dbc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Resource(name = "jdbc/hiber")
@@ -42,7 +55,15 @@ public class dbc extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Part filePart = request.getPart("file");
+	    String fileName = filePart.getSubmittedFileName();
+	    for (Part part : request.getParts()) {
+	      part.write("~/Desktop" + fileName.substring(fileName.lastIndexOf("/")+1));
+	    }
+	    response.getWriter().print("The file uploaded sucessfully.");
 
 	}
 
